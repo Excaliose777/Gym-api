77aedf6e69f04cee559e4e72444794aa1d7910c1
Alaminxab <alaminxab@gmail.com>
2023-03-08 18:09:18 +0000

    more API fetch for similar exercises

--- src/pages/ExerciseDetail.js
+++ src/pages/ExerciseDetail.js
@@ -10,6 +10,8 @@ const ExerciseDetail = () => {
 
   const [exerciseDetail, setExerciseDetail] = useState({});
   const [exerciseVideos, setExerciseVideos] = useState([]);
+  const [targetMuscleExercises, setTargetMuscleExercises] = useState([]);
+  const [equipmentExercises, setEquipmentExercises] = useState([]);
   const { id } = useParams()
 
   useEffect(() =>{
@@ -24,7 +26,12 @@ const ExerciseDetail = () => {
 
       setExerciseVideos(exerciseVideosData.contents)
 
-      // const targetMuscleExercisesData
+      const targetMuscleExercisesData = await fetchData(`${exerciseDbUrl}/exercises/target/${exerciseDetailData.target}`, exerciseOptions)
+
+      setTargetMuscleExercises(targetMuscleExercisesData)
+
+      const equipmentExercisesData = await fetchData(`${exerciseDbUrl}/exercises/target/${exerciseDetailData.equipment}`, exerciseOptions)
+      setEquipmentExercises(equipmentExercisesData)
     }
 
     fetchExercisesData();
@@ -34,7 +41,7 @@ const ExerciseDetail = () => {
     <Box>
       <Detail exerciseDetail={exerciseDetail}/>
       <ExerciseVideos exerciseVideos={exerciseVideos} name={exerciseDetail.name}/>
-      <SimilarExercises/>
+      <SimilarExercises targetMuscleExercises={targetMuscleExercises} equipmentExercises={equipmentExercises}/>
     </Box>
   )
 }
